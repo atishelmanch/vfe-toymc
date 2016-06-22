cca6275c87a8f289e3b2bba839cf8e8497ca7925
Suzannah Fraker <suzannah.fraker@gmail.com>
2016-06-22 11:19:21 -0400

    Update scan.py to write csv files and include more stats

--- reconstruction/plot/scan.py
+++ reconstruction/plot/scan.py
@@ -1,157 +1,75 @@
-from ROOT import TH1F, TFile
+import csv
+from ROOT import TFile
 from glob import glob
-from math import sqrt, log
+from math import sqrt
 from sys import argv
 
 
-def main(files, outfile_name="results.txt"):
+def main(files, outfile_name="results.csv", in_time_bx=4):
 
-    # The files to write histograms and raw text data too
+    # Output csv file
     outputfile = open(outfile_name, 'w')
-    #outrootfile = TFile("energy_hists.root", "RECREATE")
+    outputwriter = csv.writer(outputfile)
 
-    # The number of bins to use in the histogram
-    #BINS = 200
-    BINS = 4000
-    start_bin = -2
-    end_bin = 2
-    bin_size = 1.0 * (end_bin - start_bin) / BINS
-
-    # The in-time bunch crossing number. Note: This will need to be changed if
-    # in the future, changing the location of the sampling window is explored
-    in_time_bx = 4
-
-    # Logging information headers, only used for writing to output file
-    stat_names = ["NSAMPLE", "NFREQ", "Amplitude", "Pileup", "Noise"]
-    dev_names  = ["sigma_eff", "sigma_eff_err"]
-    for name in stat_names + dev_names:
-        outputfile.write('{0:>17} '.format(name))
-    outputfile.write('\n')
-    for i in enumerate(stat_names + dev_names):
-        outputfile.write('{0:->17} '.format(""))
-    outputfile.write('\n')
+    # Logging information headers used for writing to output file
+    param_names = ["nEvents", "pulse_shift", "pileup_shift", "nSmpl", "nFreq",
+                   "amplitudeTruth", "nPU", "sigmaNoise", "puFactor"]
+    reco_stat_names  = ["avg_reco_amplitude", "sigma_eff", "sigma_eff_err"]
+    outputwriter.writerow(param_names + reco_stat_names)
 
     # Accessing each file and making the histograms
-    print "\nProcessing ", len(files), "file(s)\n"
+    print "\nProcessing %d file(s)\n" % len(files)
     for i, f in enumerate(files):
 
         # Accessing the file
         print "File", i+1, ":", f
         in_file = TFile(f, "Read")
         tree = in_file.Get("RecoAndSim")
-        entries = tree.GetEntries()
+        nEvents = tree.GetEntries()
 
-        # Getting the stats for the file
+        # Getting the params for the file
         tree.GetEntry(0)
-        stats = [tree.nSmpl, tree.nFreq, tree.signalTruth , 
-                 tree.nPU, tree.sigmaNoise]
+        params = [nEvents, tree.pulse_shift, tree.pileup_shift,
+                  tree.nSmpl, tree.nFreq, tree.amplitudeTruth,
+                  tree.nPU, tree.sigmaNoise, tree.puFactor]
 
-        # Making the energy distribution histogram
-        dev_hist_name = "Hist"
-        dev_hist = TH1F("", "", BINS, start_bin, end_bin)
-        dev_list = []
-        for event in range(0, entries):
+        for name, value in zip(param_names, params):
+          print '{0:>17}: {1:>5}'.format(name, value)
+
+        # Getting the reconstructed amplitudes
+        reco_list = []
+        for event in range(nEvents):
             tree.GetEntry(event)
-            event_amplitude = tree.amplitudeTruth
-            reco_amplitude = tree.samplesReco.at(in_time_bx)
-            dev_hist.Fill(reco_amplitude - event_amplitude)
-            dev_list.append(reco_amplitude - event_amplitude)
-
-        # Filling the array for determining sigma_eff
-        # Note: the first and last bins are overflow and currently this loop
-        # includes those two bins
-        dev_array = []
-        for bin in range(0, BINS + 1):
-            dev_array.append(dev_hist.GetBinContent(bin))
+            reco_list.append(tree.samplesReco.at(in_time_bx))
 
         # calculate uncertainty
-        #sigma = sigma_eff(dev_array, bin_size, dev_hist.GetEntries())
-        sigma = sigma_eff_list(dev_list)#
-        #sigma = get_sigma(dev_hist)#
-        #return
+        reco_stats = mean_sigma_eff(reco_list)
 
-        # Logging information and terminal print outs
-        for stat, name in zip(stats, stat_names):
-            dev_hist_name += ("_" + name + "_" + str(round(stat, 3)))
-            print '{0:>17}: {1:>5}'.format(name, stat)
-            outputfile.write('{0:>17.7f} '.format(stat))
+        for name, value in zip(reco_stat_names, reco_stats):
+            print '{0:>17}: {1:>11}'.format(name, value)
 
-        for dev, name in zip(sigma, dev_names):
-            print '{0:>17}: {1:>11}'.format(name, dev)
-            outputfile.write('{0:>17.7f} '.format(dev))
-        outputfile.write("\n")
+        outputwriter.writerow(params + reco_stats)
 
-        #outrootfile.cd()
-        dev_hist.SetName(dev_hist_name)
-        dev_hist.SetTitle(dev_hist_name)
-        dev_hist.Write()
         in_file.Close()
 
     outputfile.close()
-    #outrootfile.Close()
-
-# Takes a 1D histogram, fits a Gaussian distribution to it, and
-# and returns the standard deviation calculated by the fit.
-def get_sigma(hist):
-  #result_ptr = hist.Fit("gaus", "S")
-  result_ptr = hist.Fit("gaus", "S", "", -0.5, 0.5)
-  params = result_ptr.Parameters()
-  const, mean, sigma = params
-  sigma_error = result_ptr.ParError(2)
-  #print sigma, sigma_error
-  return sigma, sigma_error
-
-# Takes in an array and calculates the effective sigma, the windowed
-# range which contains 68% of the data (one sigma). Returns a tuple 
-# containing (sigma, sigma_error).
-def sigma_eff(arr, bin_size, entries):
-    length = len(arr)
-
-    window = [0, length]
-    window_size = window[1] - window[0]
-
-    for start_bin in range(0, length - 1):
-        sum_in_range = 0
-        for end_bin in range(start_bin, length - 1):
-            sum_in_range += arr[end_bin]
-            if (sum_in_range >= 0.68 * entries) and \
-               (end_bin - start_bin < window_size):
-                window[0] = start_bin
-                window[1] = end_bin
-                window_size = end_bin - start_bin
-                break
-        else:
-            # No window was found so we can drop out of the loop
-            # Note: this is supposed to be in-line with the for-loop,
-            # not the if statement
-            break
-
-    real_start = window[0]*bin_size - 2
-    real_end = window[1]*bin_size - 2
-
-    print "Window size of:", window_size, "Between (" + \
-    str(real_start) + ", " + str(real_end) + ")"
-
-
-    sigma = (window[1] - window[0])*bin_size / 2.0
-    sigma_err = sigma / sqrt(2.0*(entries - 1.0))
-
-    return (sigma, sigma_err)
-
 
 # Takes in a list and calculates the effective sigma, the windowed
 # range which contains 68% of the data (one sigma). Returns a tuple 
 # containing (sigma, sigma_error).
-def sigma_eff_list(amp_list):
+def mean_sigma_eff(amp_list):
     amp_list = sorted(amp_list)
     n_entries = len(amp_list)
+
+    mean = sum(amp_list) / n_entries
+
     one_sigma = 0.68 * n_entries
 
     last_start_value, last_end_value = amp_list[-1], amp_list[0]
     window_size = last_start_value - last_end_value
-    print window_size
 
     for start_index in range(0, n_entries - 1):
+        found_window = False
         start_value = amp_list[start_index]
         sum_in_range = 0
         for end_index in range(start_index, n_entries):
@@ -159,24 +77,22 @@ def sigma_eff_list(amp_list):
             sum_in_range += 1
             if (sum_in_range >= one_sigma) and \
                (end_value - start_value < window_size):
+                found_window = True
                 last_start_value, last_end_value = start_value, end_value
-                window_size = last_start_value - last_end_value
+                window_size = last_end_value - last_start_value
                 break
-        else:
-            # No window was found so we can drop out of the loop
-            # Note: this is supposed to be in-line with the for-loop,
-            # not the if statement
+        if not found_window:
+            # No window was found so we can drop out of the outer loop
             break
 
     print "Window size of %f, between (%f, %f)." % (
-        window_size, last_start_value, last_end_value)
+         window_size, last_start_value, last_end_value)
 
 
     sigma = window_size / 2.0
     sigma_err = sigma / sqrt(2.0*(n_entries - 1.0))
 
-    return [sigma, sigma_err]
-
+    return [mean, sigma, sigma_err]
 
 if __name__ == "__main__":
     
