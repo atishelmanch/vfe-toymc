7b37fb1f498d44093aa9461d9c82a7ded27f2a21
Suzannah Fraker <suzannah.fraker@gmail.com>
2016-06-23 17:14:58 -0400

    cleaning up plot_sigma.py

--- reconstruction/plot/plot_sigma.py
+++ reconstruction/plot/plot_sigma.py
@@ -4,20 +4,28 @@ import csv
 from ROOT import *
 
 
-def get_data(csv_input, values, outer_keys):
+def get_data(csv_input, value_names, outer_keys):
   reader = csv.DictReader(csv_input)
   data = collections.defaultdict(list)
   for row in reader:
-    key = tuple(row[k] for k in outer_keys)
-    value = tuple(row[v] for v in values)
+    key = tuple(float(row[k]) for k in outer_keys)
+    value = tuple(float(row[v]) for v in value_names)
     data[key].append(value)
   return data
 
 
-# data[(Nsample, Nfreq)] = [(shift, amplitude), (shift, amplitude), ...]
+# data = [(x, y, y_err), (x, y, y_err), ...]
+def make_graph_line(data):
+  graph_line = TGraphErrors()
+  for i, (x, y, y_err) in enumerate(sorted(data)):
+    graph_line.SetPoint(i, x, y)
+    graph_line.SetPointError(i, 0, y_err)
+  return graph_line
 
 
-def make_one_graph(data, waveform="unknown", x_axis="Shift", title=""):
+# data[(Nsample, Nfreq)] = [(x, y, y_err), (x, y, y_err), ...]
+def make_multigraph(data, out_root_file=None, title="", x_axis_title="",
+                   save_canvas_as=""):
 
 
   lines = [(10.0, 25.0), (20.0, 12.5), (40., 6.25)]
@@ -26,24 +34,19 @@ def make_one_graph(data, waveform="unknown", x_axis="Shift", title=""):
 
   # First we iterate through all combos to make points/lines to fill
   for line_idx, line_val in enumerate(lines):
-      # Initialize the line with error bars
-      graph_line = TGraphErrors()
-      graph_line.SetName("gr_%f_%f" % (line_val))
+      graph_line = make_graph_line(data[line_val])
+      graph_line.SetName("gr_%.2f_%.2f" % (line_val))
+
       graph_line.SetMarkerColor(100 - (line_idx*12)%50)
       graph_line.SetLineColor(100 - (line_idx*12)%50)
       graph_line.SetMarkerStyle(line_idx+20)
       graph_line.SetMarkerSize(2)
-
-      for point_idx, (x_val, y_val) in enumerate(sorted(data[line_val])):
-          if abs(x_val) < 5:
-            graph_line.SetPoint(
-                point_idx, x_val, y_val)
-
       graph_line.Draw("ALP")
-      graph_line.Write()
-      g_lines.append([deepcopy(graph_line), 
-                      "NSAMPLE = " + str(line_val[0]) + 
-                      " NFREQ = " + str(line_val[1])])
+
+      if out_root_file:
+        graph_line.Write()
+
+      g_lines.append([graph_line, "NSAMPLE=%d NFREQ=%.2f" % line_val])
 
 
   # The canvas to be used 
@@ -62,25 +65,22 @@ def make_one_graph(data, waveform="unknown", x_axis="Shift", title=""):
       graph.Add(g[0])
       legend.AddEntry(g[0], g[1], "p")
   graph.Draw("APL")
-  graph.GetXaxis().SetTitle("shift (ns)")
+  graph.GetXaxis().SetTitle(x_axis_title)
   legend.Draw()
-  graph.Write()
-  if waveform.startswith("QIE"):
-    waveform = "QIE"
-  canvas.SaveAs(
-      "amplitude_vs_%s_%s.png" % (x_axis.lower(), waveform))
+  if out_root_file:
+    graph.Write()
+  if save_canvas_as:
+    canvas.SaveAs(save_canvas_as)
 
 
-wfs = ["QIE6", "QIE12", "QIE25", "CRRC10", "CRRC20", "CRRC30",
-       "CRRC43", "CRRC60", "CRRC90"]
-wfs = ["QIE*", "CRRC10", "CRRC20", "CRRC30",
-       "CRRC43", "CRRC60", "CRRC90"]
+def main(infile_name="results.csv", outfile_name="plot_sigma.root", save_canvas_as=""):
+  f = open(infile_name, "r")
+  d = get_data(f, ("sigmaNoise", "sigma_eff", "sigma_eff_err"), ("nSmpl", "nFreq"))
+  f.close()
 
-all_datas = []
+  outrootfile = TFile(outfile_name, "RECREATE")
+  make_multigraph(d, outrootfile, save_canvas_as=save_canvas_as)
+  outrootfile.Close()
 
 
-f = open("results.csv", "r")
-d = get_data(f, ("sigmaNoise", "sigma_eff"), ("nSmpl", "nFreq"))
-for k, v in d.iteritems():
-  print k
-  print v
+main()
